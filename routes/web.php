383d367216f69a46cb52590eb87c4eaa5012ebae
yuyamitsu <92497779+yuyamitsu@users.noreply.github.com>
2026-06-05 15:01:09 +0900

    認証ルート設定

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\InquiryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [InquiryController::class, 'create'])->name('inquiries.create');
 Route::post('/inquiries', [InquiryController::class, 'store'])->name('inquiries.store');
 
-Route::get('/admin/inquiries', [InquiryController::class, 'index'])->name('admin.inquiries.index');
-Route::get('/admin/inquiries/{inquiry}', [InquiryController::class, 'show'])->name('admin.inquiries.show');
-Route::put('/admin/inquiries/{inquiry}', [InquiryController::class, 'update'])->name('admin.inquiries.update');
-Route::delete('/admin/inquiries/{inquiry}', [InquiryController::class, 'destroy'])->name('admin.inquiries.destroy');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.store');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/inquiries', [InquiryController::class, 'index'])->name('inquiries.index');
+    Route::get('/inquiries/{inquiry}', [InquiryController::class, 'show'])->name('inquiries.show');
+    Route::put('/inquiries/{inquiry}', [InquiryController::class, 'update'])->name('inquiries.update');
+    Route::delete('/inquiries/{inquiry}', [InquiryController::class, 'destroy'])->name('inquiries.destroy');
+});
