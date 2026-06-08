0dde21b498d563271ed564267434ca47d6a1edf4
yuyamitsu <92497779+yuyamitsu@users.noreply.github.com>
2026-06-08 13:18:14 +0900

    ルート設定

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::get('/', [InquiryController::class, 'create'])->name('inquiries.create');
     Route::post('/inquiries', [InquiryController::class, 'store'])->name('inquiries.store');
+    Route::post('/inquiries/{inquiry}/comments', [InquiryController::class, 'storeComment'])->name('inquiries.comments.store');
 });
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
